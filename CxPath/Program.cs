c6e9948e6bbe5e7d86f850571d03026dd67580dc
Alberto Simões <Alberto.Simoes@checkmarx.com>
2022-04-26 15:20:37 +0100

    Fix internal path, and wrong named class

--- CxPath/Program.cs
+++ CxPath/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 CxPathParser parser = new CxPathParser();
-List<Query> qList = parser.ParseQuery("//IfElseStmt.then//InvokeMethodExpr[shortName='foo']");
-foreach (Query q in qList)
+List<CxPathQuery> qList = parser.ParseQuery("//IfElseStmt.then//InvokeMethodExpr[shortName='foo']");
+foreach (CxPathQuery q in qList)
 {
     Console.WriteLine(q);
 }
